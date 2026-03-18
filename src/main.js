7fc041fe764cde33036b24571418bc1efe76a5fa
Md Sarfaraz Afzal <sarfaraz.afzal@cbnits.com>
2026-03-18 16:59:05 +0530

    shifted from plugin to own logger package

--- src/main.js
+++ src/main.js
@@ -4,13 +4,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 import router from "./router";
-import myPlugin from "./plugins/myPlugin";
+import sarfarazVueLogger from "sarfaraz-vue-logger";
+// import myPlugin from "./plugins/myPlugin";
 
 const pinia = createPinia();
 const app = createApp(App);
 
 app.use(pinia);
 app.use(router);
-app.use(myPlugin);
+// app.use(myPlugin);
+app.use(sarfarazVueLogger);
 
 app.mount("#app");
--- src/plugins/myPlugin.js
+++ src/plugins/myPlugin.js
@@ -12,25 +12,24 @@ export default {
     //     document.getElementById("app").removeChild(notification);
     //   }, 5000);
     // });
-
-    app.provide("logger", {
-      logStyle:
-        "font-size:20px; font-weight:bold; background: #000000; padding: 5px; border-radius: 10px; margin: 5px",
-      success(msg) {
-        console.log(`%c${msg}`, `color:#27F52E; ${this.logStyle}`);
-      },
-      error(msg) {
-        console.log(`%c${msg}`, `color:#E50B0B; ${this.logStyle}`);
-      },
-      table(obj) {
-        console.table(obj);
-      },
-      info(msg) {
-        console.log(`%c${msg}`, `color:#0B78E5; ${this.logStyle}`);
-      },
-      warn(msg) {
-        console.log(`%c${msg}`, `color:#E5E10B; ${this.logStyle}`);
-      },
-    });
+    // app.provide("logger", {
+    //   logStyle:
+    //     "font-size:20px; font-weight:bold; background: #000000; padding: 5px; border-radius: 10px; margin: 5px",
+    //   success(msg) {
+    //     console.log(`%c${msg}`, `color:#27F52E; ${this.logStyle}`);
+    //   },
+    //   error(msg) {
+    //     console.log(`%c${msg}`, `color:#E50B0B; ${this.logStyle}`);
+    //   },
+    //   table(obj) {
+    //     console.table(obj);
+    //   },
+    //   info(msg) {
+    //     console.log(`%c${msg}`, `color:#0B78E5; ${this.logStyle}`);
+    //   },
+    //   warn(msg) {
+    //     console.log(`%c${msg}`, `color:#E5E10B; ${this.logStyle}`);
+    //   },
+    // });
   },
 };
